bacdae2dc7ee7a40a531c3d78b0498f75456ca9d
aiswaryaonline <aiswaryaonline@gmail.com>
2020-04-28 21:44:48 +0530

    Added DataStructure module

---
+++ DataStructures/DataStructures/Array.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace DataStructures
+{
+    class Array
+    {
+        public int[] InitializeArrays()
+        {
+            int[] numbers = new int[5];
+
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            numbers[3] = 4;
+            numbers[4] = 5;
+
+            for (int i = 0; i < numbers.Count(); i++)
+                Console.WriteLine($"Number {i}");
+
+            return numbers;
+        }
+
+        public int[] InitializeArraysBettweWay()
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < numbers.Count(); i++)
+                Console.WriteLine($"Number {i}");
+
+            return numbers;
+        }
+
+        public void AccessArrays()
+        {
+            var numbers = InitializeArraysBettweWay();
+
+            Console.WriteLine($"Access item index 0: {numbers[0]}");
+            Console.WriteLine($"Access item index 1: {numbers[1]}");
+            Console.WriteLine($"Access item index 2: {numbers[2]}");
+            Console.WriteLine($"Access item index 3: {numbers[3]}");
+            Console.WriteLine($"Access item index 4: {numbers[4]}");
+        }
+        // http://zetcode.com/lang/csharp/arrays/
+
+    }
+}
---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayObject = new Array();
+            arrayObject.InitializeArrays();
+            arrayObject.InitializeArraysBettweWay();
+            arrayObject.AccessArrays();
+
+            Console.ReadLine();
+        }
+    }
+}
